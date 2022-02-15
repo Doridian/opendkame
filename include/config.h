1ca374a1b3863d24f5a8c8c1309c8e691c97a2dc
Mark Dietzer <git@doridian.net>
2022-02-15 13:05:58 -0800

    wrong pinout

--- include/config.h
+++ include/config.h
@@ -2,8 +2,8 @@
 #define PIN_MISO 4
 #define PIN_MOSI 3
 #define PIN_CS 5
-#define PIN_GDO0 28
-#define PIN_GDO2 29
+#define PIN_GDO0 29
+#define PIN_GDO2 28
 
 #define PIN_NEOPIXEL 12
 #define PIN_NEOPIXEL_POWER 11
